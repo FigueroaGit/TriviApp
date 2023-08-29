bfdaa0fc00ca57437500e8f727e862dff86b40f3
FigueroaGit <hmfp24@gmail.com>
2023-08-29 12:47:41 -0600

    Setup folders and libraries

--- app/src/main/java/com/figueroa/triviapp/MainActivity.kt
+++ app/src/main/java/com/figueroa/triviapp/MainActivity.kt
@@ -3,12 +3,9 @@ package com.figueroa.triviapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.figueroa.triviapp.ui.theme.TriviAppTheme
 
@@ -19,7 +16,6 @@ class MainActivity : ComponentActivity() {
             TriviAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colorScheme.background) {
-                    
                 }
             }
         }
@@ -30,6 +26,5 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     TriviAppTheme {
-
     }
 }
